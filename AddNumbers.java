b53c948ea24eaf93f3b6a05813c4ce61dbed237b
vinaybeerpuri <beerpuriv@gmail.com>
2025-11-11 11:48:52 +0530

    Initial commit

--- AddNumbers.java
+++ AddNumbers.java
@@ -4,6 +4,6 @@ public class AddNumbers {
         int b = 20;
         int sum = a + b;
 
-        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        System.out.println("Hello World");
     }
 }
